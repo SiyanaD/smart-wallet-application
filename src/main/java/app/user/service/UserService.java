52d2ab07e4f62db3c6be3b4b50790f1d610fa9cb
Siyana Danchilova <120177143+SiyanaD@users.noreply.github.com>
2025-01-26 13:56:11 +0200

    update

--- src/main/java/app/user/service/UserService.java
+++ src/main/java/app/user/service/UserService.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 //easy for log
 @Slf4j
@@ -98,5 +99,9 @@ public class UserService {
     public List<User> getAllUsers(){
         return userRepository.findAll();
     }
+public User getById(UUID id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new DomainException("User with id [%s] does not exist."));
+}
 
 }
--- src/main/java/app/web/IndexController.java
+++ src/main/java/app/web/IndexController.java
@@ -1,15 +1,28 @@
 package app.web;
 
+import app.user.model.User;
+import app.user.service.UserService;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
 
 
 @Controller
 @RequestMapping
 public class IndexController {
-@GetMapping("/")
+    private final UserService userService;
+
+    @Autowired
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
     public String getIndexPage(){
 
         return "index";
@@ -28,8 +41,14 @@ public class IndexController {
     }
 
     @GetMapping("/home")
-    public String getHomePage(){
+    public ModelAndView getHomePage(){
+        //
+       User user= userService.getById(UUID.fromString("ab0b5242-6b5c-4d76-8c9f-124be662b2d9"));
 
-        return "home";
+
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("home");
+    modelAndView.addObject("user",user);
+        return modelAndView;
     }
 }
